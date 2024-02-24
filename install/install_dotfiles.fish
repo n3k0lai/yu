0b131b0dc5538c4d7b7b381714e56053e6bef879
Nicholai <nicholai@comfy.sh>
2024-02-24 12:40:47 -0500

    yu install is its own section

---
+++ install/install_dotfiles.fish
@@ -0,0 +1,20 @@
+function install_dotfiles
+    # args
+    #   -a --auto
+    #   -g --github
+    #   -d --doom
+    #   -o --os
+    #   -g --graphical
+    #   -h
+    # ensure git is installed
+    # download dotfiles
+    # mv dotfiles
+    # mv git folder
+    # install yu
+    # install deps
+    # download doomdir
+    # download doomemacs
+    # check for find and ripgrep
+    # install doom
+    # download org
+end
